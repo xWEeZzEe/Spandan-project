d2e2cad52852784ad7e4896554517dce6e3c43fc
xWEeZzEe <shrey.bhatnagar@srms.ac.in>
2025-04-16 11:23:03 +0530

    hii

--- Frontend/src/ExportForm.jsx
+++ Frontend/src/ExportForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { exportToExcel } from "./exportToExcel";
+import { exportToExcel } from "./ExportToExcel";
 
 const ExportForm = () => {
   const [endpoint, setEndpoint] = useState("");
